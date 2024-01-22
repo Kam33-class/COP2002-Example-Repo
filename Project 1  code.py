2956d655ecb62b8e3bfb15fb9972004b0cfd4efb
Kam33-class <157400254+Kam33-class@users.noreply.github.com>
2024-01-22 12:23:34 -0500

    Add files via upload

---
+++ Project 1  code.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Kameron Kinsman")
+Kameron Kinsman
+>>> print("COP2002-OM1")
+COP2002-OM1
+>>> print("Spring 2024")
+Spring 2024
+>>> print("This is an interactive session!")
+This is an interactive session!
